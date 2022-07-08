eb97a23ce4c2ce852c3217c43981c1c677d317cb
Dan Pososhin <pososhin.dan@gmail.com>
2022-07-08 15:22:45 +0300

    /

--- 23_old.js
+++ 23_old.js
@@ -1,7 +1,19 @@
-let n = prompt("введите любое число больше 2", "");
-lable: for (let x = 2; x <= n; x++) {
-  for (let y = 2; y < x; y++) {
+let n = prompt('Введите число больше 2','');
+lable: for (let x = 2, x <= n, x++) {
+for (let y = 2, y < x, y++) {
     if (x % y == 0) continue lable;
+}
+alert (x);
+}
+
+let n = 10;
+
+nextPrime:
+for (let i = 2; i <= n; i++) { // Для всех i...
+
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
   }
-  alert(x);
+
+  alert( i ); // простое число
 }
